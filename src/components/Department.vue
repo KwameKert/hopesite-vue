39cc41f4eb67987f5e004e2fac43fe591d8df015
kwamkert <tomriddle477@gmail.com>
2020-06-07 11:21:18 +0000

    department added

---
+++ src/components/Department.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p class="lead">Department</p>
+    </div>
+</template>
+
+
+<script lang="ts">
+
+import {Vue, Component} from 'vue-property-decorator';
+
+@Component
+export default class Department extends Vue{
+    
+}
+</script>
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -12,13 +12,11 @@
 
 <script lang="ts">
 
-import Header from "../components/Header.vue";
-import Misc from "../components/Misc.vue";
-import Footer from "../components/Footer.vue";
+import Header from "@/components/Header.vue";
+import Misc from "@/components/Misc.vue";
+import Footer from "@/components/Footer.vue";
 
 import {Vue, Component, Prop}  from 'vue-property-decorator';
-//import  Header from '@/components/Header';
-//import {Header} from "../components/Header"
 
 @Component({
     components: {
